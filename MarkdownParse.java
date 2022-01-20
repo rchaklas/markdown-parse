46b3b68bfc96f8c62f10088869849681b21de64a
rchaklas <66855264+rchaklas@users.noreply.github.com>
2022-01-19 17:14:07 -0800

    debugging some cases

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,15 +11,25 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            System.out.println(currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+
+            if (nextOpenBracket < 0 || nextCloseBracket < 0 || 
+                openParen < 0 || closeParen < 0) {
+                break;
+            }
+
+            if(markdown.charAt(nextOpenBracket - 1) == '!' ||
+                nextCloseBracket != openParen - 1) {
+                currentIndex = closeParen + 1;
+                continue;
+            }
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
-        System.out.println(currentIndex);
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
