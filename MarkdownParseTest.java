ae57e24e66a15edebfa2a9546927893217b978cf
rchaklas <66855264+rchaklas@users.noreply.github.com>
2022-02-24 23:57:06 -0800

    lab-report-4
    added snippet files and tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -84,4 +84,25 @@ public class MarkdownParseTest {
         List<String> expect = List.of("");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
+
+    @Test
+    public void testSnippet1() throws IOException {
+        String contents = Files.readString(Path.of("./snippet1.md"));
+        List<String> expect = List.of("`google.com, google.com, ucsd.edu");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        String contents = Files.readString(Path.of("./snippet2.md"));
+        List<String> expect = List.of("a.com, a.com(()), example.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        String contents = Files.readString(Path.of("./snippet3.md"));
+        List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
 }
